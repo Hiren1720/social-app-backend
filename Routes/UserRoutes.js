ca92bd3a47d6f7222b07a589c56fbda901fea374
Hiren1722 <hirenbhuva2000@gmail.com>
2023-03-23 23:20:07 +0530

    Add Mail functionality & OTP verification

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,9 +1,10 @@
-const { GetAll,getById,Register,Login,Update,Delete,LogOut,generateAccessToken  } = require("../Controllers/UserController");
+const { GetAll,getById,Register,Login,VerifyOTP,Update,Delete,LogOut,generateAccessToken  } = require("../Controllers/UserController");
 const router = require("express").Router();
 const auth = require("../Middleware/Auth");
 
 router.post("/register", Register);
 router.post("/login", Login);
+router.post("/verify-otp", VerifyOTP);
 router.get("/profile/:id",auth, getById);
 router.post("/logout",auth, LogOut);
 router.get("/userAll",auth, GetAll);
