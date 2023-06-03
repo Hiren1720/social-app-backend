c36d3b14d0e2d9ef5eacd2e04d71367c117e99eb
sonupal <s4sonupal14@gmail.com>
2023-06-03 21:34:13 +0530

    update and delete functionality of post

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -17,6 +17,41 @@ module.exports.createPost = async (req, res) => {
 
     }
 };
+
+
+module.exports.updatePost = async (req, res) => {
+    try {
+    //     let postData = JSON.parse(req.body?.post);
+    //     let post = new Post({...postData,imageUrl: req?.file?.filename ? `/Posts/${req?.file?.filename}`:''});
+    //     post.save(function (error, document) {
+    //         if (error) {
+    //             res.status(400).send({success: false, msg: "Request Failed", data: error});
+    //         } else {
+    //             res.status(201).send({success: true, msg: "Post Created", data: document});
+    //         }
+    //     });
+    // } catch (ex) {
+    //
+    // }
+
+        let data = JSON.parse(req.body?.post);
+        console.log("data", data)
+        let post = new Post({...data,imageUrl: req?.file?.filename ? `/Posts/${req?.file?.filename}`:''});
+        // if(!req?.body?.profile)
+        // {
+        //     data.profile_url= `/Posts/${req?.file?.filename}`
+        // }
+        let postData = await Post.findOneAndUpdate({_id:data._id}, post).lean();
+
+        if (postData) {
+            return res.status(201).send({success: true, msg: "Post Updated Successfully",data:'document'});
+        } else {
+            return res.status(400).send({success: false, msg: "Post Update Failed", data: null});
+        }
+    } catch (ex) {
+        res.send(ex);
+    }
+};
 module.exports.getAllPost = async (req, res) => {
     try {
         let post = await Post.aggregate([
@@ -58,6 +93,9 @@ module.exports.getAllPost = async (req, res) => {
         res.status(400).send({success: false, msg: "", error: ex});
     }
 };
+
+
+
 module.exports.getMentionPosts = async (req, res) => {
     try {
         let {id} = req?.params;
@@ -84,6 +122,26 @@ module.exports.getMentionPosts = async (req, res) => {
         res.status(400).send({success: false, msg: "", error: ex});
     }
 };
+
+module.exports.deletePost = async  (req, res)=>{
+    try{
+        if (!req.params.id) {
+            return res.json({ msg: "User id is required " });
+        }
+        else {
+            console.log("reaefads", req.params)
+            let post = await Post.deleteOne({_id:req.params.id})
+            if(post){
+                return res.status(200).send({success: true,msg:"Post Deleted Successfully",});
+            }
+            else{
+                return res.status(400).send({success: false,msg:"failed",});
+            }
+        }
+    } catch (ex) {
+        res.send(ex);
+    }
+}
 module.exports.getAllLikes = async (req, res) => {
     try {
         let users = await User.find({
--- Routes/PostRoutes.js
+++ Routes/PostRoutes.js
@@ -1,13 +1,16 @@
-const { createPost, getAllPost, postLike,getAllLikes,getMentionPosts,getPost } = require("../Controllers/PostController");
+
+const { createPost, getAllPost, postLike,getAllLikes,getMentionPosts,deletePost,updatePost,getPost} = require("../Controllers/PostController");
 const router = require("express").Router();
 const auth = require("../Middleware/Auth");
 const multer = require('multer');
 const {storageEngine} = require('../Utils/helper');
 const upload = multer({ storage: storageEngine('Posts') });
 router.post("/create",auth,upload.single('postImage'),createPost);
+router.post("/update",auth,upload.single('postImage'),updatePost);
 router.get("/getAllPost",auth, getAllPost);
 router.get("/:id/getPost",auth, getPost);
 router.post("/postLike",auth, postLike);
 router.post("/getLikes",auth, getAllLikes);
 router.get("/getMentionPosts/:id",auth, getMentionPosts);
+router.post("/deletePost/:id", auth, deletePost)
 module.exports = router;
