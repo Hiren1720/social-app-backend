43292844a2a136b55751df9f8c4ebca8e077a6ec
Hiren1722 <hirenbhuva2000@gmail.com>
2023-07-12 23:30:59 +0530

    Code refactor && remove unnecessary api calls

--- Routes/PostRoutes.js
+++ Routes/PostRoutes.js
@@ -1,5 +1,5 @@
 
-const { createPost, getAllPost, postLike,getAllLikes,getMentionPosts,deletePost,updatePost,getPost,getSavedPost,savePost} = require("../Controllers/PostController");
+const { createPost, getAllPost, postLike,getAllLikes,getMentionPosts,deletePost,updatePost,getPost,getSavedPost,savePost,getPostByUserId} = require("../Controllers/PostController");
 const { createCommentOnPost,getCommentsById } = require("../Controllers/CommentController");
 const router = require("express").Router();
 const auth = require("../Middleware/Auth");
@@ -9,12 +9,13 @@ const upload = multer({ storage: storageEngine('Posts') });
 router.post("/create",auth,upload.array('postImage'),createPost);
 router.post("/update",auth,upload.array('postImage'),updatePost);
 router.get("/getAllPost",auth, getAllPost);
-router.get("/:id/getPost",auth, getPost);
+router.get("/getPost",auth, getPost);
+router.get("/getPostsByUserId",auth, getPostByUserId);
 router.post("/postLike",auth, postLike);
 router.get("/likes",auth, getAllLikes);
 router.get("/getMentionPosts/:id",auth, getMentionPosts);
 router.post("/deletePost/:id", auth, deletePost);
-router.get("/savedPosts",auth, getSavedPost);
+router.get("/getSavedPosts",auth, getSavedPost);
 router.post("/savePost",auth, savePost);
 router.post("/create",auth, createCommentOnPost);
 router.get("/comments",auth, getCommentsById);
