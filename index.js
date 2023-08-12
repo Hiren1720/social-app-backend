29f48991bdb5c5cb71430dbe98030e51a22b4d7f
Hiren1722 <hirenbhuva2000@gmail.com>
2023-08-12 14:24:27 +0530

    fix sse issue

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const handleSSE = (req,res) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_APP_URL);
     const clientId = Date.now();
     const newClient = {
         id: clientId,
