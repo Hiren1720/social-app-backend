2df9a0b140e511b3f0faf86f2b815a4bc52bc8a0
Hiren1722 <hirenbhuva2000@gmail.com>
2023-08-16 23:13:12 +0530

    fix sse issue

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const handleSSE = (req,res) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
-    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_APP_URL);
+    res.setHeader('Access-Control-Allow-Origin', 'https://social-v1-app.vercel.app');
     const clientId = Date.now();
     const newClient = {
         id: clientId,
